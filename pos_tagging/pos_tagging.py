cd2af0ddade0be075072ce9fdb97d34ea1d1688d
geffroy <thomas.geffroy@ensta-paristech.fr>
2019-03-05 20:37:00 +0100

    Ajout du pos tagging ntlk StanfordPOS de 2017

---
+++ pos_tagging/pos_tagging.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+
+#IMPORTS AND FUNCTION DEFINITIONS
+
+#NLTK
+
+#imports
+import nltk #import the natural language toolkit library
+from nltk import RegexpTokenizer
+from nltk.stem.snowball import FrenchStemmer #import the French stemming library
+from nltk.corpus import stopwords #import stopwords from nltk corpus
+
+import os #import os module
+PATH = os.path.abspath(os.path.join(os.getcwd(), ".."))
+
+#reading in the raw text from the file
+def read_raw_file(path):
+    '''reads in raw text from a text file using the argument (path), which represents the path/to/file'''
+    f = open(path,"r") #open the file located at "path" as a file object (f) that is readonly
+    raw = f.read().decode('utf8') # read raw text into a variable (raw) after decoding it from utf8
+    f.close() #close the file now that it isn;t being used any longer
+    return raw
+
+def get_nltk_text(raw,tokenizer,encoding='utf8'):
+    '''create an nltk text using the passed argument (raw) after filtering out the commas'''
+    #turn the raw text into an nltk text object
+    text = tokenizer.tokenize(raw)
+    return text
+
+def get_stopswords(type="veronis"):
+    '''returns the veronis stopwords in unicode, or if any other value is passed, it returns the default nltk french stopwords'''
+    if type=="veronis":
+        #VERONIS STOPWORDS
+        raw_stopword_list = ["Ap.", "Apr.", "GHz", "MHz", "USD", "a", "afin", "ah", "ai", "aie", "aient", "aies", "ait", "alors", "après", "as", "attendu", "au", "au-delà", "au-devant", "aucun", "aucune", "audit", "auprès", "auquel", "aura", "aurai", "auraient", "aurais", "aurait", "auras", "aurez", "auriez", "aurions", "aurons", "auront", "aussi", "autour", "autre", "autres", "autrui", "aux", "auxdites", "auxdits", "auxquelles", "auxquels", "avaient", "avais", "avait", "avant", "avec", "avez", "aviez", "avions", "avons", "ayant", "ayez", "ayons", "b", "bah", "banco", "ben", "bien", "bé", "c", "c'", "c'est", "c'était", "car", "ce", "ceci", "cela", "celle", "celle-ci", "celle-là", "celles", "celles-ci", "celles-là", "celui", "celui-ci", "celui-là", "celà", "cent", "cents", "cependant", "certain", "certaine", "certaines", "certains", "ces", "cet", "cette", "ceux", "ceux-ci", "ceux-là", "cf.", "cg", "cgr", "chacun", "chacune", "chaque", "chez", "ci", "cinq", "cinquante", "cinquante-cinq", "cinquante-deux", "cinquante-et-un", "cinquante-huit", "cinquante-neuf", "cinquante-quatre", "cinquante-sept", "cinquante-six", "cinquante-trois", "cl", "cm", "cm²", "comme", "contre", "d", "d'", "d'après", "d'un", "d'une", "dans", "de", "depuis", "derrière", "des", "desdites", "desdits", "desquelles", "desquels", "deux", "devant", "devers", "dg", "différentes", "différents", "divers", "diverses", "dix", "dix-huit", "dix-neuf", "dix-sept", "dl", "dm", "donc", "dont", "douze", "du", "dudit", "duquel", "durant", "dès", "déjà", "e", "eh", "elle", "elles", "en", "en-dehors", "encore", "enfin", "entre", "envers", "es", "est", "et", "eu", "eue", "eues", "euh", "eurent", "eus", "eusse", "eussent", "eusses", "eussiez", "eussions", "eut", "eux", "eûmes", "eût", "eûtes", "f", "fait", "fi", "flac", "fors", "furent", "fus", "fusse", "fussent", "fusses", "fussiez", "fussions", "fut", "fûmes", "fût", "fûtes", "g", "gr", "h", "ha", "han", "hein", "hem", "heu", "hg", "hl", "hm", "hm³", "holà", "hop", "hormis", "hors", "huit", "hum", "hé", "i", "ici", "il", "ils", "j", "j'", "j'ai", "j'avais", "j'étais", "jamais", "je", "jusqu'", "jusqu'au", "jusqu'aux", "jusqu'à", "jusque", "k", "kg", "km", "km²", "l", "l'", "l'autre", "l'on", "l'un", "l'une", "la", "laquelle", "le", "lequel", "les", "lesquelles", "lesquels", "leur", "leurs", "lez", "lors", "lorsqu'", "lorsque", "lui", "lès", "m", "m'", "ma", "maint", "mainte", "maintes", "maints", "mais", "malgré", "me", "mes", "mg", "mgr", "mil", "mille", "milliards", "millions", "ml", "mm", "mm²", "moi", "moins", "mon", "moyennant", "mt", "m²", "m³", "même", "mêmes", "n", "n'avait", "n'y", "ne", "neuf", "ni", "non", "nonante", "nonobstant", "nos", "notre", "nous", "nul", "nulle", "nº", "néanmoins", "o", "octante", "oh", "on", "ont", "onze", "or", "ou", "outre", "où", "p", "par", "par-delà", "parbleu", "parce", "parmi", "pas", "passé", "pendant", "personne", "peu", "plus", "plus_d'un", "plus_d'une", "plusieurs", "pour", "pourquoi", "pourtant", "pourvu", "près", "puisqu'", "puisque", "q", "qu", "qu'", "qu'elle", "qu'elles", "qu'il", "qu'ils", "qu'on", "quand", "quant", "quarante", "quarante-cinq", "quarante-deux", "quarante-et-un", "quarante-huit", "quarante-neuf", "quarante-quatre", "quarante-sept", "quarante-six", "quarante-trois", "quatorze", "quatre", "quatre-vingt", "quatre-vingt-cinq", "quatre-vingt-deux", "quatre-vingt-dix", "quatre-vingt-dix-huit", "quatre-vingt-dix-neuf", "quatre-vingt-dix-sept", "quatre-vingt-douze", "quatre-vingt-huit", "quatre-vingt-neuf", "quatre-vingt-onze", "quatre-vingt-quatorze", "quatre-vingt-quatre", "quatre-vingt-quinze", "quatre-vingt-seize", "quatre-vingt-sept", "quatre-vingt-six", "quatre-vingt-treize", "quatre-vingt-trois", "quatre-vingt-un", "quatre-vingt-une", "quatre-vingts", "que", "quel", "quelle", "quelles", "quelqu'", "quelqu'un", "quelqu'une", "quelque", "quelques", "quelques-unes", "quelques-uns", "quels", "qui", "quiconque", "quinze", "quoi", "quoiqu'", "quoique", "r", "revoici", "revoilà", "rien", "s", "s'", "sa", "sans", "sauf", "se", "seize", "selon", "sept", "septante", "sera", "serai", "seraient", "serais", "serait", "seras", "serez", "seriez", "serions", "serons", "seront", "ses", "si", "sinon", "six", "soi", "soient", "sois", "soit", "soixante", "soixante-cinq", "soixante-deux", "soixante-dix", "soixante-dix-huit", "soixante-dix-neuf", "soixante-dix-sept", "soixante-douze", "soixante-et-onze", "soixante-et-un", "soixante-et-une", "soixante-huit", "soixante-neuf", "soixante-quatorze", "soixante-quatre", "soixante-quinze", "soixante-seize", "soixante-sept", "soixante-six", "soixante-treize", "soixante-trois", "sommes", "son", "sont", "sous", "soyez", "soyons", "suis", "suite", "sur", "sus", "t", "t'", "ta", "tacatac", "tandis", "te", "tel", "telle", "telles", "tels", "tes", "toi", "ton", "toujours", "tous", "tout", "toute", "toutefois", "toutes", "treize", "trente", "trente-cinq", "trente-deux", "trente-et-un", "trente-huit", "trente-neuf", "trente-quatre", "trente-sept", "trente-six", "trente-trois", "trois", "très", "tu", "u", "un", "une", "unes", "uns", "v", "vers", "via", "vingt", "vingt-cinq", "vingt-deux", "vingt-huit", "vingt-neuf", "vingt-quatre", "vingt-sept", "vingt-six", "vingt-trois", "vis-à-vis", "voici", "voilà", "vos", "votre", "vous", "w", "x", "y", "z", "zéro", "à", "ç'", "ça", "ès", "étaient", "étais", "était", "étant", "étiez", "étions", "été", "étée", "étées", "étés", "êtes", "être", "ô"]
+    else:
+        #get French stopwords from the nltk kit
+        raw_stopword_list = stopwords.words('french') #create a list of all French stopwords
+    stopword_list = [word for word in raw_stopword_list] #make to decode the French stopwords as unicode objects rather than ascii
+    return stopword_list
+    
+
+def filter_stopwords(text,stopword_list):
+    '''normalizes the words by turning them all lowercase and then filters out the stopwords'''
+    words=[w.lower() for w in text] #normalize the words in the text, making them all lowercase
+    #filtering stopwords
+    filtered_words = [] #declare an empty list to hold our filtered words
+    for word in words: #iterate over all words from the text
+        if word not in stopword_list and word.isalpha() and len(word) > 1: #only add words that are not in the French stopwords list, are alphabetic, and are more than 1 character
+            filtered_words.append(word) #add word to filter_words list if it meets the above conditions
+    filtered_words.sort() #sort filtered_words list
+    return filtered_words
+    
+def stem_words(words):
+    '''stems the word list using the French Stemmer'''
+    #stemming words
+    stemmed_words = [] #declare an empty list to hold our stemmed words
+    stemmer = FrenchStemmer() #create a stemmer object in the FrenchStemmer class
+    for word in words:
+        stemmed_word=stemmer.stem(word) #stem the word
+        stemmed_words.append(stemmed_word) #add it to our stemmed word list
+    stemmed_words.sort() #sort the stemmed_words
+    return stemmed_words
+   
+
+
+        
+from nltk.tag.stanford import StanfordPOSTagger #Get the Part of Speech tagger from NLP at Stanford, python module that interacts with Java
+nltk.internals.config_java("/usr/bin/java", options='-mx1000m',verbose=False) #set the path to java (note: i had to edit stanford.py and comment conflicting settings on lines 59 and 85
+
+tag_abbreviations = {
+                    'A': 'adjective',
+                    'Adv': 'adverb',
+                    'CC': 'coordinating conjunction',
+                    'Cl': 'weak clitic pronoun',
+                    'CS': 'subordinating conjunction',
+                    'D': 'determiner',
+                    'ET': 'foreign word',
+                    'I': 'interjection',
+                    'NC': 'common noun',
+                    'NP': 'proper noun',
+                    'P': 'preposition',
+                    'PREF': 'prefix',
+                    'PRO': 'strong pronoun',
+                    'V': 'verb',
+                    'PONCT': 'punctuation mark',
+                    'N': 'noun'}
+
+def pos_tag(to_tag,pos_tagger):
+    '''tag the tokens with part of speech; to_tag is the tags; model_path is the file path to the stanford POS tagger model; and jar_path to the Stanford POS tagger jar file'''
+    tags = pos_tagger.tag(to_tag) #run the tagging algorithm on the tokenized raw text
+    return tags
+
+def print_pos_tags(tags):
+    '''print all the tags with their part of speech; tag[0] is the word; tag[1] is the Part of Speech'''
+    for tag in tags: print(tag[1]+'\t',tag[0]) 
+            
+            
+
+
+# LOAD THE CSV FILE AND POS TAG
+import pandas as pd
+#variables
+data_path= PATH + "/data.csv" #set the path that we are going to read through
+tagged_coms=[] #open up an empty array into which we will store our tagged coms
+
+datas = pd.read_csv(data_path)
+
+tokenizer = RegexpTokenizer(r'''\w'|\w+|[^\w\s]''')
+    
+model_path = PATH + "/pos_tagging/stanford-postagger-full-2017-06-09/models/french.tagger"
+jar_path = PATH + "/pos_tagging/stanford-postagger-full-2017-06-09/stanford-postagger.jar"
+pos_tagger = StanfordPOSTagger(model_path,jar_path,encoding='utf8') #create an object of class POSTagger that is encoded in UTF-8
+
+numb_com = len(datas)    
+for ind in range(numb_com):
+    print(float(ind/numb_com))
+    com = datas["review_txt"][ind]
+    if type(com)==str:
+        text = get_nltk_text(com,tokenizer)
+        tagged_coms.append(pos_tag(text,pos_tagger))
+
+#Add the result to the database (in csv format)
+df = pd.DataFrame({'pos_tagged_coms':tagged_coms})
+df.to_csv(PATH+"/pos_tagging/pos_tagged.csv")
